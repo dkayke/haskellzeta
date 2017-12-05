b061ee6e2138d5915a967a7c548bf6f51ddd184b
romefeller <henrikeze@gmail.com>
2017-12-05 01:42:05 +0000

    (FINAL) Atualização do Post e Validação do CadVeiculo

--- src/Handler/CadVeiculo.hs
+++ src/Handler/CadVeiculo.hs
@@ -22,7 +22,48 @@ formVeiculo = renderDivs $ (,) <$> (Veiculo
     <*> areq textField "CNH do Motorista" Nothing
     
 getCadVeiculoR :: Handler Html
-getCadVeiculoR = undefined
+getCadVeiculoR = do 
+    (widgetVeic,enctype) <- generateFormPost formVeiculo
+    msg <- getMessage
+    layoutParkSec "CadVeiculoR" 
+        [whamlet|
+            <nav>
+                <div class="breadcrumb">
+                    <div class="col s12 mdl-color--grey-100">
+                        <span>HaskPark /
+                        <a href=@{CadastroR}>Cadastro <span>/
+                        <a>Veiculo
+             $maybe mensagem <- msg 
+                <h2> #{mensagem}
+            <form action=@{CadVeiculoR} enctype=#{enctype} method=post>
+                ^{widgetVeic}
+                    <button class="mdl-button mdl-js-button mdl-button--raised bt-acao">Cadastrar veiculo
+        |]
 
 postCadVeiculoR :: Handler Html
-postCadVeiculoR = undefined
+postCadVeiculoR = do 
+    ((res,_),_) <- runFormPost formVeiculo
+    case res of
+        FormSuccess (veiculo, cnh) -> do 
+            placaveic <- runDB $ selectFirst [VeiculoPlaca ==. veiculoPlaca veiculo][] 
+            case placaveic of
+                Just resultadoplaca -> do
+                    setMessage $ [shamlet| Placa ja cadastrada |]
+                    redirect CadVeiculoR
+                _ -> do
+                    cnhcli <- runDB $ selectFirst [ClienteCnh ==. cnh][]
+                    case cnhcli of 
+                        Just resultadocnh -> do
+                            _ <- runDB $ insert $ Veiculo (veiculoTipoveic veiculo) 
+                                                          (veiculoPlaca veiculo)
+                                                          (veiculoUfplaca veiculo)
+                                                          (entityKey resultadocnh)
+                                                          (veiculoModelo veiculo)
+                                                          (veiculoAno veiculo)
+                                                          (veiculoCor veiculo)
+                            setMessage $ [shamlet| Veículo cadastrado com sucesso |]
+                            redirect CadVeiculoR
+                        _ -> do
+                            setMessage $ [shamlet| Motorista não cadastrado |]
+                            redirect CadVeiculoR
+        _ -> redirect CadVeiculoR
