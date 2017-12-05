c88d4ec16f3d2014ad3a5b0abacf9b85f4876ddb
romefeller <henrikeze@gmail.com>
2017-12-05 01:36:49 +0000

    (FINAL) Atualização do Post e Validação do CadCliente

--- src/Handler/CadCliente.hs
+++ src/Handler/CadCliente.hs
@@ -20,7 +20,36 @@ formCliente = renderDivs $ Cliente
     <*> aopt intField "Telefone 2 " Nothing
 
 getCadClienteR :: Handler Html
-getCadClienteR = undefined
+getCadClienteR = do 
+    (widgetCli,enctype) <- generateFormPost formCliente
+    msg <- getMessage
+    layoutParkSec "CadClienteR" 
+        [whamlet|
+            <nav>
+                <div class="breadcrumb">
+                    <div class="col s12 mdl-color--grey-100">
+                        <span>HaskPark /
+                        <a href=@{CadastroR}>Cadastro <span>/
+                        <a>Cliente
+             $maybe mensagem <- msg 
+                <h2> #{mensagem}
+            <form action=@{CadClienteR} enctype=#{enctype} method=post>
+                ^{widgetCli}
+                    <button class="mdl-button mdl-js-button mdl-button--raised bt-acao">Cadastrar cliente
+        |]
 
 postCadClienteR :: Handler Html
-postCadClienteR = undefined
+postCadClienteR = do 
+    ((res,_),_) <- runFormPost formCliente
+    case res of 
+        FormSuccess cliente -> do 
+            cnhcli <- runDB $ selectFirst [ClienteCnh ==. clienteCnh cliente][]
+            case cnhcli of
+                Just resultadocnh -> do
+                    setMessage $ [shamlet| CNH ja cadastrada por outro motorista |]
+                    redirect CadClienteR
+                _ -> do
+                    _ <- runDB $ insert cliente
+                    setMessage $ [shamlet| Cliente cadastrado com sucesso |]
+                    redirect CadClienteR
+        _ -> redirect CadClienteR
