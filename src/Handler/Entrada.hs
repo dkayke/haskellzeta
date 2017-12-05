2522c4fad076e8d24ee9168abf588e85f2132da8
romefeller <henrikeze@gmail.com>
2017-12-05 01:44:16 +0000

    Criação do Form de Entrada

--- src/Handler/Entrada.hs
+++ src/Handler/Entrada.hs
@@ -9,48 +9,15 @@ module Handler.Entrada where
 
 import Import
 import LayoutPark
-import Data.Time.Clock
-import Data.Time.LocalTime
-import System.IO.Unsafe
-import Database.Persist.Postgresql
 
-formEntrada :: Form (Int, Text)
-formEntrada = renderDivs $ (,)
-    <$> areq intField "CNH" Nothing
-    <*> areq textField "Placa do Veículo" Nothing
+formEntrada :: Form (TipoentradaId, ClienteId, VeiculoId)
+formEntrada = renderDivs $ (, , )
+    <$> areq (selectField $ optionsPersistKey [] [] tipoentradaNome) "Selecione o tipo de entrada:" Nothing
+    <*> areq (selectField $ optionsPersistKey [] [] clienteCnh) "CNH" Nothing
+    <*> areq (selectField $ optionsPersistKey [] [] veiculoPlaca) "Placa do Veículo" Nothing
 
-getEntradaR :: Handler Html -- entrando com formulario 
-getEntradaR = do 
-    (widget,enctype) <- generateFormPost formEntrada
-    msg <- getMessage
-    layoutPark $ do 
-        [whamlet|
-            <nav>
-                <div class="breadcrumb">
-                    <div class="col s12 mdl-color--grey-100">
-                        <span>HaskPark /
-                        <a>Entrada
-                    
-            <div class="msg-erro-login">
-                $maybe mensagem <- msg 
-                    <p> #{mensagem}
-            <form action=@{EntradaR} enctype=#{enctype} method=post>
-                ^{widget}
-                <button type="submit" value="entrada" class="mdl-button mdl-button-login mdl-js-button mdl-button--raised">Entrar
-        |]
-        
+getEntradaR :: Handler Html
+getEntradaR = undefined
+
 postEntradaR :: Handler Html
-postEntradaR = do 
-    ((res,_),_) <- runFormPost formEntrada
-    dthora <- liftIO getCurrentTime
-    case res of 
-        FormSuccess (cnh, placa) -> do 
-            Just cnhcli <- runDB $ selectFirst [ClienteCnh ==. cnh][]
-            Just placaveic <- runDB $ selectFirst [VeiculoPlaca ==. placa][]
-            _ <- runDB $ insert $ Entrada (entityKey cnhcli) 
-                                          (entityKey placaveic)
-                                          (dthora)
-            setMessage $ [shamlet| Entrada efetuada |]
-            redirect EntradaR
-        _ -> redirect EntradaR
-    
+postEntradaR = undefined
