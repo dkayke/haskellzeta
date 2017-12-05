b0a695b5b359be24f90618147d41d0f1d171b599
romefeller <henrikeze@gmail.com>
2017-12-05 01:41:03 +0000

    (FINAL) Atualização do Form de CadVeiculo usando Tuplas

--- src/Handler/CadVeiculo.hs
+++ src/Handler/CadVeiculo.hs
@@ -10,36 +10,19 @@ import Import
 import LayoutPark
 import Database.Persist.Postgresql
 
-formVeiculo :: Form (Veiculo, Int)
+formVeiculo :: Form (Veiculo, Text)
 formVeiculo = renderDivs $ (,) <$> (Veiculo 
-    <$> areq (selectField $ optionsPersistKey [] [] tipoveiculoNome) "Tipo Veiculo " Nothing
+    <$> areq (selectField $ optionsPersistKey [] [] tipoveiculoNome) "Tipo Veículo " Nothing
     <*> areq textField "Placa " Nothing
     <*> areq textField "UF - Cidade da Placa " Nothing
     <*> pure (toSqlKey 0)
     <*> aopt textField "Modelo do Veiculo" Nothing
     <*> aopt intField "Ano " Nothing
     <*> aopt textField "Cor " Nothing)
-    <*> areq intField "CNH do Motorista" Nothing
+    <*> areq textField "CNH do Motorista" Nothing
     
 getCadVeiculoR :: Handler Html
-getCadVeiculoR = do 
-    (widgetVeic,enctype) <- generateFormPost formVeiculo
-    msg <- getMessage
-    layoutParkSec "CadVeiculoR" 
-        [whamlet|
-            <nav>
-                <div class="breadcrumb">
-                    <div class="col s12 mdl-color--grey-100">
-                        <span>HaskPark /
-                        <a href=@{CadastroR}>Cadastro <span>/
-                        <a>Veiculo
-             $maybe mensagem <- msg 
-                <h1> #{mensagem}
-            <form action=@{CadVeiculoR} enctype=#{enctype} method=post>
-                ^{widgetVeic}
-                    <button class="mdl-button mdl-js-button mdl-button--raised bt-acao">Cadastrar veiculo
-        |]
+getCadVeiculoR = undefined
 
 postCadVeiculoR :: Handler Html
 postCadVeiculoR = undefined
-    
