ad05131902685ccf1e2f15da557196b82a46bc29
romefeller <viniciusangelucci@hotmail.com>
2017-12-05 20:32:24 +0000

    Criação do GetBusca

--- src/Handler/Busca.hs
+++ src/Handler/Busca.hs
@@ -18,9 +18,27 @@ data Pesq = Pesq
 formBusca :: Form Pesq
 formBusca = renderDivs $ Pesq
     <$> areq textField "CNH ou placa do veículo" Nothing
-    
+
 getBuscaR :: Handler Html
-getBuscaR = undefined
+getBuscaR = do 
+    (widget,enctype) <- generateFormPost formBusca
+    msg <- getMessage
+    layoutPark $ do 
+        [whamlet|
+            $maybe mensagem <- msg 
+                    <div id="snackbar"> #{mensagem}
+                    <script> myFunction();
+                    
+            <nav>
+                <div class="breadcrumb">
+                    <div class="col s12 mdl-color--grey-100">
+                        <span>HaskPark /
+                        <a>Busca
+                
+            <form action=@{BuscaR} enctype=#{enctype} method=post>
+                ^{widget}
+                <button type="submit" class="mdl-button mdl-button-login mdl-js-button mdl-button--raised">Pesquisar
+        |]
 
 postBuscaR :: Handler Html
 postBuscaR = undefined 
